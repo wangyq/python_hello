f5920d88cb29a38738cd56a04073bab873adb5d6
Yinqing Wang <yinqingwang@gmail.com>
2015-06-15 00:10:39 +0800

    add python bottle's demo in openshift platform

---
+++ wx_openshift/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Hello', version='1.0',
+      description='Hello world application',
+      author='Your Name', author_email='ramr@example.org',
+      url='http://www.python.org/sigs/distutils-sig/',
+
+      #  Uncomment one or more lines below in the install_requires section
+      #  for the specific client drivers/modules your application needs.
+      #install_requires=['bottle==0.12.8']
+     )
