e2afa472ab420b04a2ffd2ae061cf473c996a38f
Yinqing Wang <yinqingwang@gmail.com>
2015-06-17 12:36:41 +0800

    ok

--- crawl_web.py
+++ crawl_web.py
@@ -9,6 +9,63 @@ __license__ = 'MIT'
 
 import os, urllib2,urllib,re
 
+
+class  GirlTaoBaoMM(object): 
+    def __init__(self,picLocalPath,page_num=50): 
+        self.page_num = page_num
+        self.picLocalPath = picLocalPath
+        self.mmurl= "http://mm.taobao.com/json/request_top_list.htm?type=0&page="
+    def  download(self): 
+        i = 1
+        page_num = self.page_num
+        picLocalPath = self.picLocalPath
+
+        temp  ='''<img src="'''
+        while i<page_num: 
+            url = self.mmurl + str(i) 
+            up =  urllib2.urlopen(url) 
+            cont = up.read() 
+            pa = j = 0
+            while True: 
+                ahref = '''<a href="http'''
+                target = "target"
+                pa = cont.find(ahref)
+                pt = cont.find(target, pa) 
+                if pa == -1:
+                    break
+                modelurl = cont[pa+len(ahref)-4: pt-2] 
+                mup=  urllib2.urlopen(modelurl) 
+                mcont = mup.read() 
+    
+                header = "<img style"
+                tail = ".jpg"
+                iph = k = 0
+                while True: 
+                    iph = mcont.find(header) 
+                    ipj  =  mcont.find(tail,  iph) 
+                    if iph == -1:
+                        break      
+                    mpic = mcont[iph : ipj + len(tail)] 
+                    ips = mpic.find("src") 
+                    urlpic =  mpic[ips +len("src ="):]
+                    try: 
+                        print ">>>downloading : lady_p"+str(i)+"_no_"+str(j)+"_pic_"+str(k)+".jpg......"
+                        #urllib.urlretrieve(urlpic,  "lady_p"+str(i)+"_no_"+str(j)+"_pic_"+str(k)+".jpg") 
+                        urllib.urlretrieve(urlpic,  os.path.join(picLocalPath,"lady_p"+str(i)+"_no_"+str(j)+"_pic_"+str(k)+".jpg")) 
+                    except KeyboardInterrupt: 
+                        print "SIGINT, exit..."
+                        sys.exit(0) 
+                    except: 
+                        pass
+                    mcont = mcont[ipj+1:]
+                    k+=1
+                cont = cont[pt+1:]
+                j+=1
+            i += 1
+        print ">>>download completed"
+
+
+
 def crawl_tuigirl(storageDir, count = 50):
     tmmurl ="http://www.rouruan.com/models/mdown/"
 
@@ -50,5 +107,7 @@ def crawl_tuigirl(storageDir, count = 50):
 #  main():
 #
 if __name__ == '__main__':
-    crawl_tuigirl("/home/wang/data/tuigirl")
+    #crawl_tuigirl("/home/wang/data/pci/tuigirl")
+    mm = GirlTaoBaoMM(50,"/home/wang/data/pic/taobaomm")
+    mm.download()
     pass
