3b42718f54710c2ef62423d885cbf8fe45e33f7c
Yinqing Wang <yinqingwang@gmail.com>
2015-11-02 17:34:46 +0800

    ad

---
+++ pingpong.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a  demo 
+"""
+__author__ = 'yinqingwang@163.com'
+__version__ = '0.10'
+__license__ = 'MIT'
+
+import datetime,time
+ 
+sz = 5000000
+ 
+def pong():
+    v = yield
+    while v >= 0:
+        v2 = v + 1
+        v = yield v2
+    return
+ 
+def ping(peer):
+    cur_val = 0
+    while cur_val <= sz:
+        new_val = peer.send(cur_val)
+        cur_val = new_val + 1
+ 
+    print(cur_val)
+ 
+ 
+n2 = pong()
+t1 = datetime.datetime.now() 
+n2.send(None)   # start it so it pauses on the first yield
+ 
+#t1 = datetime.datetime.now()
+ping(n2)
+t2 = datetime.datetime.now() 
+ 
+d = (t2-t1).microseconds 
+print("%d ms" %d)
